dfc3ec941ceae693ccf2773c9daf9966c3deb1c6
quapka <quapka@gmail.com>
2016-10-10 23:18:55 +0200

    printing messages in a box

---
+++ boxed_msg.py
@@ -0,0 +1,25 @@
+from math import ceil, floor
+
+def boxed_msg(msg):
+    lines = msg.split('\n')
+    max_length = max([len(line) for line in lines])
+    horizontal = '+' + '-' * (max_length + 2) + '+\n'
+    res = horizontal
+    for l in lines:
+        res += format_line(l, max_length)
+    res += horizontal
+    return res.strip()
+    
+def format_line(line, max_length):
+    # 'a', 3 -> '  a  '
+    # 'a', 4 ->   '   a  '
+    # 'abcd, 4 -> ' abcd '
+    half_dif = (max_length - len(line)) / 2 # in Python 3.x float division
+    return '| ' + ' ' * ceil(half_dif) + line + ' ' * floor(half_dif) + ' |\n'
+
+print(boxed_msg('first_line\nsecond_line\nthe_ultimate_longest_line'))
+# +---------------------------+
+# |         first_line        |
+# |        second_line        |
+# | the_ultimate_longest_line |
+# +---------------------------+
